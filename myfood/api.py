7cdf1918bf69a59e7472d6d5d399b6cfe796c7ad
Max Malyshev <snobby.obnx@gmail.com>
2025-07-17 17:14:25 +0200

    Change to search endpoint

--- myfood/api.py
+++ myfood/api.py
@@ -1,6 +1,7 @@
 from typing import List, Optional
 
 from ninja import NinjaAPI, Schema
+# from ninja.security import django_auth
 from ninja.pagination import paginate
 from myfood.models import FoodProduct
 
@@ -15,8 +16,22 @@ class FoodProductSchema(Schema):
     carbohydrates_100g: Optional[float]
     proteins_100g: Optional[float]
 
+# class FoodProductDetailSchema(FoodProductSchema):
+#     fiber_100g: Optional[float]
+#     sugars_100g: Optional[float]
 
-@api.get('/foodproducts/', response=List[FoodProductSchema])
+@api.get('/foodproducts/search/', response=List[FoodProductSchema])
 @paginate
-def list_food_products(request):
-    return FoodProduct.objects.all()
+def search_food_products(request, q: Optional[str] = None):
+    qs = FoodProduct.objects.all()
+    if q:
+        qs = qs.filter(product_name__icontains=q)
+    return qs
+
+# @api.get('/foodproducts/search_detailed/', response=List[FoodProductSchema])
+# @paginate
+# def search_detailed_food_products(request, q: Optional[str] = None, auth=django_auth):
+#     qs = FoodProduct.objects.all()
+#     if q:
+#         qs = qs.filter(product_name__icontains=q)
+#     return qs
--- myfood/tests/test_api_foodproducts.py
+++ myfood/tests/test_api_foodproducts.py
@@ -12,7 +12,7 @@ def test_pagination():
 
     # Testing default pagination from NINJA_PAGINATION_PER_PAGE setting
     response = client.get(
-        path='/api/foodproducts/',
+        path='/api/foodproducts/search/',
         content_type='application/json',
     )
     data = response.json()
@@ -22,9 +22,29 @@ def test_pagination():
     # Testing custom limit
     limit = 30
     response = client.get(
-        path='/api/foodproducts/',
+        path='/api/foodproducts/search/',
         content_type='application/json',
         data={'limit': limit}
     )
     data = response.json()
     assert len(data['items']) == limit
+
+@pytest.mark.django_db
+def test_search_food_products_q_param():
+    # Create products with and without 'eggs' in the name
+    FoodProductFactory.create_batch(5, product_name="eggs")
+    FoodProductFactory.create_batch(3, product_name="Eggs Benedict")
+    FoodProductFactory.create_batch(10, product_name="milk")
+    client = Client()
+
+    response = client.get(
+        path='/api/foodproducts/search/',
+        data={'q': 'eggs'},
+        content_type='application/json',
+    )
+    data = response.json()
+    # All items with 'eggs' (case-insensitive) in product_name should be returned
+    assert data['count'] == 8
+    product_names = [item['product_name'] for item in data['items']]
+    for name in product_names:
+        assert 'eggs' in name.lower()
