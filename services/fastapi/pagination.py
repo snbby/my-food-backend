1eb16cf8ed651ef196658f0a713e7fd5bccfff16
Max Malyshev <snobby.obnx@gmail.com>
2025-07-18 16:56:51 +0200

    Refactored FastAPI

---
+++ services/fastapi/pagination.py
@@ -0,0 +1,11 @@
+# pagination.py
+from fastapi import Query
+
+class PaginationParams:
+    def __init__(
+        self,
+        limit: int = Query(30, ge=1, le=100),
+        offset: int = Query(0, ge=0)
+    ):
+        self.limit = limit
+        self.offset = offset
