51f30012f9ae52c4521b7483cda74fbfc31c5156
Max Malyshev <snobby.obnx@gmail.com>
2025-08-15 14:40:52 +0200

    Add some style and few new columns

--- services/frontend/src/App.tsx
+++ services/frontend/src/App.tsx
@@ -1,10 +1,14 @@
 import React, { useEffect, useState } from 'react';
+import './table.css'
 
 interface Product {
   id: number;
   product_name: string;
   brands: string;
   countries: string;
+  carbohydrates_100g: number;
+	proteins_100g: number;      
+	fat_100g: number;
 }
 
 const App: React.FC = () => {
@@ -25,7 +29,10 @@ const App: React.FC = () => {
           <tr>
             <th>Product Name</th>
             <th>Brands</th>
-            <th>Countries</th>
+            <th>Country</th>
+            <th>Carbohydrates</th>
+            <th>Proteins</th>
+            <th>Fat</th>
           </tr>
         </thead>
         <tbody>
@@ -34,6 +41,9 @@ const App: React.FC = () => {
               <td>{p.product_name}</td>
               <td>{p.brands}</td>
               <td>{p.countries}</td>
+              <td>{p.carbohydrates_100g}</td>
+              <td>{p.proteins_100g}</td>
+              <td>{p.fat_100g}</td>
             </tr>
           ))}
         </tbody>
