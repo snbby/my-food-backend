53e3c26c0708da6319e04f0ddede17108399ee38
Max Malyshev <max909907@gmail.com>
2025-08-15 11:43:35 +0200

    Add React frontend service

---
+++ services/frontend/src/App.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect, useState } from 'react';
+
+interface Product {
+  id: number;
+  product_name: string;
+  brands: string;
+  countries: string;
+}
+
+const App: React.FC = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch('https://golang.my-food.com/api/foodproducts/search/?limit=30')
+      .then((res) => res.json())
+      .then((data) => setProducts(data))
+      .catch((err) => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <h1>My-food</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Product Name</th>
+            <th>Brands</th>
+            <th>Countries</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((p) => (
+            <tr key={p.id}>
+              <td>{p.product_name}</td>
+              <td>{p.brands}</td>
+              <td>{p.countries}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default App;
